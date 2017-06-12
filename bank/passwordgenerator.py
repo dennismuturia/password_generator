85c53c0e5a93660845f3f90c90d36526ad99e911
dennismuturia <dmuturia2922@gmail.com>
2017-06-12 07:17:13 +0300

    This is a password generator

--- bank/passwordgenerator.py
+++ bank/passwordgenerator.py
@@ -1,9 +1,15 @@
 #/usr/bin/python
+'''
+Author: Dennis Muturia
+version: v1.0.0
+'''
+import sqlite3
+import connect
 from random import randint
 
 def randomPassword(password_length=5):
-    letters = ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','0','1', '2', '3', '4','5','6','7','8','9']
-    password = '';
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','0','1', '2', '3', '4','5','6','7','8','9']
+    password = ''
     i = 0
     while(i < password_length):
         rand = randint(0,len(letters) - 1)
